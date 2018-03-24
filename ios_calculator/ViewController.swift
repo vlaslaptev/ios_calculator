e3aa7b902f45d27a7a94a9c707520b377bbbc5df
v.laptev <v.laptev@qiwi.com>
2018-03-24 16:00:02 +0300

    Initial Commit

---
+++ ios_calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_calculator
+//
+//  Created by v.laptev on 24.03.2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
