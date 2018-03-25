709984106b842445d2a5d8c449693acf051270ff
v.laptev <v.laptev@qiwi.com>
2018-03-25 22:23:56 +0300

    add pressNumber func

--- ios_calculator/ViewController.swift
+++ ios_calculator/ViewController.swift
@@ -15,6 +15,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var isStartTyping = true;
+    
+    @IBAction func pressNumber(_ sender: UIButton) {
+        let number = sender.currentTitle!;
+        let currentText = resultLabel.text ?? ""
+        
+        if (number == "<––") {
+            if (currentText == "" || currentText.count == 1) {
+                resultLabel.text = "0"
+                isStartTyping = true
+            } else {
+                resultLabel.text = String (currentText.dropLast())
+            }
+            return;
+        }
+        
+        if (currentText.count < 25) {
+            if (isStartTyping) {
+                resultLabel.text = number
+                isStartTyping = false;
+            } else {
+                resultLabel.text = resultLabel.text! + number
+            }
+        }
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
