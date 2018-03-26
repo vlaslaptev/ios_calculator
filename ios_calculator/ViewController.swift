4ef6cbc896456cde25ce228cd42fefbb9b46c728
v.laptev <v.laptev@qiwi.com>
2018-03-26 23:26:34 +0300

    add apply operator func

--- ios_calculator/ViewController.swift
+++ ios_calculator/ViewController.swift
@@ -17,10 +17,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var resultLabel: UILabel!
     
-    var isStartTyping = true;
+    var isStartTyping = true
     
     @IBAction func pressNumber(_ sender: UIButton) {
-        let number = sender.currentTitle!;
+        let number = sender.currentTitle!
         let currentText = resultLabel.text ?? ""
         
         if (number == "<––") {
@@ -30,17 +30,72 @@ class ViewController: UIViewController {
             } else {
                 resultLabel.text = String (currentText.dropLast())
             }
-            return;
+            return
+        }
+        
+        if ((number == "0" || number == "000") && isStartTyping) {
+            return
         }
         
         if (currentText.count < 25) {
             if (isStartTyping) {
                 resultLabel.text = number
-                isStartTyping = false;
+                isStartTyping = false
             } else {
                 resultLabel.text = resultLabel.text! + number
             }
         }
+    }
+    
+    var firstOperand: Double = 0
+    var currentInput: Double {
+        get {
+            return Double(resultLabel.text!)!
+        }
+        
+        set{
+            resultLabel.text = "\(newValue)"
+            isStartTyping = true
+        }
+    }
+
+    var isPressOperator = false
+    var operatorType = ""
+    @IBAction func pressOperator(_ sender: UIButton) {
+        if (isPressOperator) {
+            var result: Double = applyOperator(operatorType: operatorType, firstOperand: firstOperand, secondOperand: currentInput)
+            currentInput = result
+            firstOperand = result
+            operatorType = sender.currentTitle!
+        } else {
+            firstOperand = currentInput
+            isStartTyping = true
+            isPressOperator = true
+            operatorType = sender.currentTitle!
+        }
+    }
+    
+    func applyOperator(operatorType: String,
+                       firstOperand: Double,
+                       secondOperand: Double) -> Double {
+        var result: Double = 0
+        switch operatorType {
+        case "+":
+            result = firstOperand + secondOperand
+            break
+        case "-":
+            result = firstOperand - secondOperand
+            break
+        case "✕":
+            result = firstOperand * secondOperand
+            break
+        case "÷":
+            result = firstOperand / secondOperand
+            break
+        default:
+            break;
+        }
+        return result;
     }
     
     
